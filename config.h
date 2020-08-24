a07273fdf6a0eb3ed01e6acf44047457fce5e90c
ybygjylj <abc1870819884@163.com>
2020-08-24 14:40:49 +0800

    test

--- config.h
+++ config.h
@@ -138,7 +138,7 @@ static Key keys[] = {
 //	{ MODKEY,              XK_m,                    setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,              XK_f,                    fullscreen,     {0} },
 	//{ MODKEY,              XK_space,                setlayout,      {0} },
-	//{ MODKEY|ShiftMask,    XK_space,                togglefloating, {0} },
+	{ MODKEY|ShiftMask,    XK_space,                togglefloating, {0} },
 	{ MODKEY,              XK_apostrophe,           togglescratch,  {.v = scratchpadcmd } },
 	{ MODKEY,              XK_0,                    view,           {.ui = ~0 } },
 	//{ MODKEY|ShiftMask,    XK_0,                    tag,            {.ui = ~0 } },
