6ff6a3b9b8e74138d2b7754e4a62d5d02c09f97a
ybygjylj <abc1870819884@gmail.com>
2020-12-13 16:51:59 +0800

    font

--- config.h
+++ config.h
@@ -17,8 +17,8 @@ static const int smartgaps          = 1;        /* 1 means no outer gap when the
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
-static const char *fonts[]          = { "JetBrainsMono \Nerd \Font \Mono:size=16" };
-static const char dmenufont[]       = "JetBrainsMono \Nerd \Font \Mono:size=16";
+static const char *fonts[]          = { "JetBrainsMono\ Nerd\ Font\ Mono:size=16" };
+static const char dmenufont[]       = "JetBrainsMono\ Nerd\ Font\ Mono:size=16";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
